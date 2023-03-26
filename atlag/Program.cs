276d9db2a8242559812647c4071da53ec963ee79
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 16:26:07 +0200

    3 szám átlaga

---
+++ atlag/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atlag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot!");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérek még egy számot!");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérek egy harmadik számot!");
+            int szam3 = Convert.ToInt32(Console.ReadLine());
+            int atlag = (szam1 + szam2 + szam3) / 3;
+            Console.WriteLine("A három szám átlaga:"  +atlag + "\n");
+
+            Console.ReadKey();
+        }
+    }
+}
